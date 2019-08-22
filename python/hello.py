67ca7d6d49e55e4276877951e1f6267c3394fc1d
NLshaen <yan.lucas@external.thalesaleniaspace.com>
2019-08-22 16:08:19 +0200

    add infra_inventory.py def printtohtml myclusterlist

--- python/hello.py
+++ python/hello.py
@@ -6,17 +6,27 @@
 from flask import Flask
 from flask import render_template
 
-app = Flask(__name__)
+from infra_inventory import NutanixApiRest
 
+app = Flask(__name__)
 
 @app.route('/')
 
 def index():
 
     var = "Flask html"
-    return render_template('infra_inventory.html', name='CAMERLO JOEY alias the snake',
-    mylist=[0,1,3]) 
+    return render_template('infra_inventory.html', name='INFRA NUTANIX INVENTORY', myCustomList=['test','another','again']) 
 
 if __name__ == '__main__':
+    
+    Nutanix = NutanixApiRest()
+ 
+    status, cluster = Nutanix.getClusterInformation()
+    print ("=" * 100)
+    print "Name: %s" % cluster.get('name')
+    print "ID: %s" % cluster.get('id')
+    print "Cluster Nodes: %s" % cluster.get('num_nodes')
+
+
 
     app.run(host='0.0.0.0', port='5000', debug=True)
--- python/infra_inventory.py
+++ python/infra_inventory.py
@@ -14,6 +14,7 @@ import random
 import sys
 import traceback
 import csv
+import datetime
 
 from collections import OrderedDict
 from operator import itemgetter, attrgetter, methodcaller
@@ -21,7 +22,7 @@ from operator import itemgetter, attrgetter, methodcaller
 #import locale
 #locale.setlocale(locale.LC_ALL, 'utf-8')
 
-#Vars
+#VARS
 
 #URL API REST NUTANIX
 #url = "https://172.30.172.179:9440/PrismGateway/services/rest/v2.0/"
@@ -29,8 +30,8 @@ from operator import itemgetter, attrgetter, methodcaller
 #echo 'import crypt,getpass; print crypt.crypt(getpass.getpass(), "$6$TASSalt")' | python -
 #$6$TASSalt$5Q3WshgOijNAohsW3va.Q/jfv/kbfziP0xVL3jNaOxNNDhQ7R/JRZjawLEM0xvQzWVDuAga6fvaPN3EKbOu1y/
 
-#Class test API REST TEST
-class TestRestApi():                
+#Class Nutanix API REST
+class NutanixApiRest():                
   def __init__(self):
     """Init the connection to the REST API Explore"""
     urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
@@ -75,29 +76,41 @@ class TestRestApi():
     return sorted(items, comparateur, reserve=True)
 
 #Define fonction print to hmtl
-  def printToHTML(self, myCustomList, htmlfile):    
+  def printToHTML(self, myClusterList, myVmList, htmlfile):    
       # DEBUG LIST
-      print "===== DEBUG ("+ str(len(myCustomList)) +") ====="
-      print str(myCustomList)
+      print "===== DEBUG ("+ str(len(myClusterList)) +") ====="
+      print ( str(myClusterList), str(myVmList) )
 
-      html =  "<html>\n<head><title>INVENTORY NUTANIX</title>\n<style>p { margin: 0 !important; } table { border: 1px solid #000; width: 500px; }</style></head>\n<body>\n"
+      datetime_object=datetime.datetime.now()      
 
-      html += '\n<p>This page was last updated on ' + "XXX" + '</p>\n'
+      html =  "<html>\n<head><title>INVENTORY NUTANIX</title>\n<style>p { margin: 0 !important; } table { border: 1px solid #000; width: 900px; }</style></head>\n<body>\n"
 
-      for item in myCustomList:
+      html += '\n<p>This page was last updated on ' + str(datetime_object) + '</p>\n'
+
+      for item in myClusterList:
+        html += '<table><tr colspan=\"2\"><th>Cluster Name</th></tr>'
+        
+        orderedItem = list()
+        orderedItem.append('clustername')
+        orderedItem.extend( sorted( (key for key in item.keys() if key not in orderedItem), key=lambda f: f[0]) )
+
+        for key in orderedItem:
+          html += '<td>'+ key +':' + str(item[key]) + '</td>'
+        html += '</table>'
+
+      for item in myVmList:
         # DEBUG LIST
         #print "===== DEBUG ITEM ====="
-        #print str(item['memory'])
+        #print str(item['vmname'])
         
         html += '<table><tr colspan=\"2\"><th>VM Name</th></tr>'
         
         orderedItem = list()
-        orderedItem.append('name')
+        orderedItem.append('vmname')
         orderedItem.extend( sorted( (key for key in item.keys() if key not in orderedItem), key=lambda f: f[0]) )
         
         for key in orderedItem:
-          html += '<tr><td>'+ key +'</td><td>' + str(item[key]) + '</td></tr>\n'
-          
+          html += '<td>'+ key +':' + str(item[key]) + '</td>'
         html += '</table>'
 
       # Finish HTML
@@ -107,7 +120,6 @@ class TestRestApi():
         f.write(html)
         f.close()
 
-
 #Execution command
 if __name__ == "__main__":
   try:
@@ -116,32 +128,65 @@ if __name__ == "__main__":
     #outputfile = open('/var/www/html/inventory/output/infra_inventory.html','w')
     #sys.stdout = open('/var/www/html/inventory/output/infra_inventory.html','w')
 
-    testRestApi = TestRestApi()   
+    Nutanix = NutanixApiRest()   
     
-    status, cluster = testRestApi.getClusterInformation()
+    status, cluster = Nutanix.getClusterInformation()
     
-    print ("=" * 100)
     print "Name: %s" % cluster.get('name')
     print "ID: %s" % cluster.get('id')
     print "Cluster Nodes: %s" % cluster.get('num_nodes')
     
     for item in cluster.get('rackable_units'):
       print ("Service Tag : " + item['serial'] + " ,Model Name : " + item['model_name'] )
-
       print "Version: %s" % cluster.get('version')
       print "Architecture: %s" % cluster.get('cluster_arch')
       hypervtype = cluster.get('hypervisor_types')
       print "Hypervisor Types: %s" % hypervtype[0]
     print ("=" * 100)
 
-    status, vm = testRestApi.getVmInformation()
+    status, vm = Nutanix.getVmInformation()
     
     numbervm = vm.get('metadata')
     print (numbervm.values())
     print ("Number of VMs on cluster: %s"  % numbervm[u'count'])
     print "List of VMs Names on cluster: "
     
-    myCustomList = list()
+    myClusterList = list()
+    
+    clustername = cluster.get('name')
+    clusterid = cluster.get('id')
+    clusternodes = cluster.get('num_nodes')
+
+    for item in cluster.get('rackable_units'):
+      clusterservicetag = item['serial']
+      clustermodelname = item['model_name']
+      clusterversion = cluster.get('version')
+      clusterarch = cluster.get('cluster_arch')
+      clusterhypervtype = cluster.get('hypervisor_types')
+
+      myClusterDictionnary = {
+        "clustername": clustername,
+        "clusterid": clusterid,
+        "clusternodes": clusternodes,
+        "clusterservicetag": clusterservicetag,
+        "clustermodelname": clustermodelname,
+        "clusterversion": clusterversion,
+        "clusterarch": clusterarch,
+        "clusterhypervtype": clusterhypervtype
+      }
+      myClusterList.append(myClusterDictionnary)
+
+    # Sort by clustername value
+    myClusterList.sort(key=lambda f: f['clustername'])
+    print "====== SORT BY CLUSTER NAME ======"
+    print( str(myClusterList) )
+
+    #Nutanix.printToHTML(myClusterList, '/var/www/html/inventory/output/infra_inventory.html' )
+
+    myVmList = list()
+
+    vmnumber = vm.get('metadata')
+
     for item in vm.get('entities'):
       vmname = item['name']
       vmpwstate = item['power_state']
@@ -149,31 +194,31 @@ if __name__ == "__main__":
       vmcore = item['num_cores_per_vcpu']
       vmmemory = item['memory_mb']/1024
       
-      myCustomDictionnary = {
-        "name": vmname,
-        "state": vmpwstate,
-        "vcpu": vmcpunumber,
-        "core": vmcore,
-        "memory": vmmemory
+      myVmDictionnary = {
+        "vmnumber": vmnumber,
+        "vmname": vmname,
+        "vmstate": vmpwstate,
+        "vmvcpu": vmcpunumber,
+        "vmcore": vmcore,
+        "vmmemory": vmmemory
       }
-      myCustomList.append(myCustomDictionnary)
+      myVmList.append(myVmDictionnary)
     
-    # Sort by memory value (cast as integer)
-    #myCustomList.sort(key=lambda f: int(f['memory']))
-    #print "====== SORT BY MEMORY ======"
-    #print( str(myCustomList) )
+    # Sort by vmmemory value (cast as integer)
+    #myVmList.sort(key=lambda f: int(f['vmmemory']))
+    #print "====== SORT BY VM MEMORY ======"
+    #print( str(myVmList) )
     
-    # Sort by name value
-    myCustomList.sort(key=lambda f: f['name'])
-    print "====== SORT BY NAME ======"
-    print( str(myCustomList) )
-    testRestApi.printToHTML(myCustomList, '/var/www/html/inventory/output/infra_inventory.html' )
+    # Sort by vmname value
+    myVmList.sort(key=lambda f: f['vmname'])
+    print "====== SORT BY VM NAME ======"
+    print( str(myVmList) )
+   
+    Nutanix.printToHTML(myClusterList, myVmList, '/var/www/html/inventory/output/infra_inventory.html' )
     
-
     print ("*" * 100)
     print "Status code: %s" % status
     print ("*" * 100)
-    #print >> outputfile, item
 
   except Exception as ex:
     print ex
